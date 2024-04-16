94f93edcab7c212fc2170c3f768d20b8b11cf97e
jakub <j.pawelski.it@gmail.com>
2024-04-16 18:12:26 +0200

    Configured swagger to support identity endpoints

--- Assets.API/Program.cs
+++ Assets.API/Program.cs
@@ -3,12 +3,36 @@ using Assets.Application.Extensions;
 using Assets.Infrastructure.Seeders;
 using Assets.Domain.Entities;
 using Assets.Infrastructure;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme, Id =  "bearerAuth"
+                },
+            },
+            []
+        }
+    });
+});
+builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
@@ -23,12 +47,15 @@ var seeder = scope.ServiceProvider.GetRequiredService<IAssetSeeders>();
 await seeder.Seed();
 
 
+  app.UseSwagger();
+  app.UseSwaggerUI();
+
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
-app.MapIdentityApi<User>();
+app.MapGroup("api/identity/").MapIdentityApi<User>();
 
 app.UseAuthorization();
 
